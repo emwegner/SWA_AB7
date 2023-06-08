f21cef11ccefad2eeec6e2b100855dbe8aee4f65
emwegner <93592205+emwegner@users.noreply.github.com>
2023-06-09 01:20:32 +0200

    small change

--- src/main/java/org/acme/Security/Startup.java
+++ src/main/java/org/acme/Security/Startup.java
@@ -16,7 +16,7 @@ public class Startup {
         // reset and load all test users
         User.deleteAll();
         User.add("admin", "admin", "admin");
-        User.add("user", "user", "user");
+      //  User.add("user", "user", "user");
         Kunde.add("Emil","Mueller", new Adresse("23232","Hausen","Wolfweg","42"));
         Kunde kunde = Kunde.findByLastname("Mueller");
         User.add("user", "user", "user", kunde.getId()) ;
